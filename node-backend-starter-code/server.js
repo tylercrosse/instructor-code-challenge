d7f7b611b12c3dc903d37668cab09d1d618e2d24
Tyler <tylerscottcrosse@gmail.com>
2015-12-05 21:20:49 -0500

    Bug fixes and file setup

--- node-backend-starter-code/server.js
+++ node-backend-starter-code/server.js
@@ -4,22 +4,25 @@ var fs = require('fs');
 var path = require('path');
 
 app.use(express.static(path.join(__dirname, '/public')));
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/', express.static(path.join(__dirname, 'public'));
+app.get('/', function(req, res) {
+  res.render('public/index.html');
+});
 
 app.get('/favorites', function(req, res){
   var data = fs.readFileSync('./data.json');
   res.setHeader('Content-Type', 'application/json');
   res.send(data);
-;
+});
 
 app.get('favorites', function(req, res){
   if(!req.body.name || !req.body.oid){
     res.send("Error");
-    return
-  
+    return;
+  }
+
   var data = JSON.parse(fs.readFileSync('./data.json'));
   data.push(req.body);
   fs.writeFile('./data.json', JSON.stringify(data));
@@ -27,6 +30,6 @@ app.get('favorites', function(req, res){
   res.send(data);
 });
 
-app.list(3000, function(){
+app.listen(3000, function(){
   console.log("Listening on port 3000");
 });
