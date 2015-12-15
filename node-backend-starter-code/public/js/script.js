ae37231b1e426eaf94371731398f0cf649004edd
Tyler <tylerscottcrosse@gmail.com>
2015-12-14 23:01:46 -0500

    Added getDetails function with redndering of details

--- node-backend-starter-code/public/js/script.js
+++ node-backend-starter-code/public/js/script.js
@@ -8,12 +8,6 @@ window.onload = function() {
     searchForm.reset();
   });
 
-  // var clearResultsForm = document.getElementById('clear-results-form');
-  // searchForm.addEventListener('submit', function() {
-  //   event.preventDefault();
-  //   popcorn.clearResults();
-  // });
-
   var showFavForm = document.getElementById('show-fav-form');
   showFavForm.addEventListener('submit', function() {
     event.preventDefault();
@@ -32,38 +26,71 @@ var popcorn = {
     // create new XMLHttpRequest Object
     var xhr = new XMLHttpRequest();
     // callback on XMLHttpRequest Object to render response to DOM
-    xhr.onload = popcorn.renderXhr;
+    xhr.onload = popcorn.renderMovies;
     // initialize XMLHttpRequest with HTTP method, where to make request, boolean for whether call is asynch (TRUE = asynch)
     xhr.open('GET', url, true);
     // sends request, if asynch returns as soon as request is sent, if not asynch then doesn't return until response has arrived
     xhr.send();
   },
-  renderXhr: function(e) {
+  renderMovies: function(e) {
     // parse XMLHttpRequest.responseText in object
-    var responseObj = JSON.parse(e.target.responseText);
+    var resObj = JSON.parse(e.target.responseText);
     // use just the 'Search' values of response object
-    var response = responseObj.Search;
-    console.log(response);
+    var res = resObj.Search;
+    console.log(res);
 
-    // create divs for each item of response, add them to '.results'
-    for (var i = 0; i < response.length; i++) {
+    // create html elements for each item of res, add them to '.results'
+    for (var i = 0; i < res.length; i++) {
       var newDiv = document.createElement('div');
-      newDiv.setAttribute('class', 'movieResult');
-      newDiv.innerHTML = '<p>'+ response[i].Title +'</p><img class="moviePoster" src='+ response[i].Poster +'>';
+      newDiv.setAttribute('id', res[i].imdbID);
+      newDiv.setAttribute('class', 'omdbResult');
+      newDiv.innerHTML =
+        '<strong><p class="title">'+ res[i].Title +'</p></strong> \
+        <p>'+ res[i].Year +'</p> \
+        <img class="moviePoster" src='+ res[i].Poster +'>';
+
+      var detailButton = document.createElement('button');
+      detailButton.innerHTML = 'Show Details';
+      detailButton.setAttribute('class', 'inlineButton detailButton');
+      detailButton.addEventListener('click', popcorn.getDetails);
 
       var favButton = document.createElement('form');
       favButton.setAttribute('class', 'inlineButton');
       favButton.innerHTML = '<input type="submit" value="Favorite this Movie" >';
       favButton.addEventListener('submit', popcorn.addToFavorites);
 
+      newDiv.appendChild(detailButton);
       newDiv.appendChild(favButton);
       results.appendChild(newDiv);
     }
   },
-  // clearResults: function() {
-  //   var results = document.getElementById('results');
-  //   results.innerHTML = '';
-  // },
+  getDetails: function() {
+    event.preventDefault();
+
+    var self = this;
+    var omdbResult = self.parentNode;
+    omdbResult.classList.add("detailed");
+    var title = omdbResult.querySelector('.title').innerHTML;
+    var url = 'http://www.omdbapi.com/?t=' + escape(title) + '&y=&plot=full&r=json';
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function(e) {
+      var res = JSON.parse(e.target.responseText);
+      popcorn.renderDetails(res, omdbResult);
+    };
+    xhr.open('GET', url, true);
+    xhr.send();
+  },
+  renderDetails: function(res, omdbResult) {
+    var details = document.createElement('div');
+    details.innerHTML =
+      '<p>'+ res.Rated +' | '+ res.Runtime +' | '+ res.Genre +'</p> \
+      <p>IMDB Rating: '+ res.imdbRating +' / 10</p> \
+      <p>Director: '+ res.Director +'</p> \
+      <p>Writer: '+ res.Writer +'</p> \
+      <p>Actors: '+ res.Actors +'</p> \
+      <p class="plot">'+ res.Plot +'</p>';
+    omdbResult.appendChild(details);
+  },
   addToFavorites: function() {
     event.preventDefault();
 
@@ -71,7 +98,7 @@ var popcorn = {
     xhr.onload = function() {
       //
     };
-    xhr.open(); // method, destination, aysnc=boolean
+    xhr.open('POST', url, true); // method, destination, aysnc=boolean
     xhr.send(); // data to send
   },
   showFavorites: function() {
